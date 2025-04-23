4c1279bc736547bcf8b43592cd493e4d5f98b638
nortveeb2408 <conv.19aivfiiyfe2h7@fleep.io>
2025-04-23 19:20:35 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Luuletus from './Luuletus'
+
+function App() {
+  return (
+      <div>
+          <h1>Luuletus</h1>
+          <Luuletus />
+          <p>esimene <br/> teine</p>
+      </div>
+  )
+}
+export default App
---
+++ src/Luuletus.jsx
@@ -0,0 +1,17 @@
+function Luuletus() {
+    return (
+        <>
+            <p>Kes hommikuti külas käib,</p>
+            <p>see asjatult ei longi</p>
+            <p>sest selleks hommik ongi</p>
+            <div className="eriline">
+                <p>kui hommikul on külm,</p>
+                <p>siis hommikul on külm</p>
+                <p>ja hommikul on külm</p>
+                <p>ja hommikul on külm</p>
+            </div>
+        </>
+    )
+}
+
+export default Luuletus
